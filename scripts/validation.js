d4d547c9c85e691f2a33d2be1234dd603c1e1c3c
Maurice <maurice.montaque@yahoo.com>
2024-10-31 14:32:40 -0400

    validation.js line51

--- scripts/validation.js
+++ scripts/validation.js
@@ -49,11 +49,11 @@ function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
   const submitButton = formEl.querySelector(options.submitButtonSelector);
+  toggleButtonState(inputEls, submitButton, options);
 
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
-      toggleButtonState(inputEls, submitButton, options);
     });
   });
 }
