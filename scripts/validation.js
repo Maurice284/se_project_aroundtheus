891a70590b5d515719d909422318d226bbd2ccac
Maurice <maurice.montaque@yahoo.com>
2024-10-31 20:27:23 -0400

    updated

--- scripts/validation.js
+++ scripts/validation.js
@@ -54,6 +54,7 @@ function setEventListeners(formEl, options) {
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputEls, submitButton, options);
     });
   });
 }
